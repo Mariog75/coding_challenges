bec0684679684a1414b9510b921116648f43c1b6
Mario Grebelski <grebelsm@dhcp-128-189-249-213.ubcsecure.wireless.ubc.ca>
2019-01-30 21:24:09 -0800

    Better isunique

--- IsUnique.py
+++ IsUnique.py
@@ -1,3 +1,4 @@
+#O(n^2)
 def isunique ( str ):
 
     for i in range(len(str)):
@@ -7,9 +8,23 @@ def isunique ( str ):
                 return 0
     
     return 1
+#O(n)
+def isuniquebetter ( str ):
+    if(len(str) > 128):
+        return 0
+    arr = [None] * 128
+
+    for ch in str:
+        if(arr[ord(ch)] != None):
+            return 0
+        else:
+            arr[ord(ch)] = 1
+    
+    return 1
+
 
 def main():
-    print(isunique( "bb" ))
+    print(isuniquebetter( "badrtyui" ))
 
 if __name__ == "__main__":
     main()
